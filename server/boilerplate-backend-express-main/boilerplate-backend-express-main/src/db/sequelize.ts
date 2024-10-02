62b747f8fef2e11e35f79d0e68589f2c3a58c0f8
Athikhom Raksaphong <athikhom_ra@outlook.com>
2024-10-02 17:18:26 +0700

    feat: add relation between user model and device model

--- server/boilerplate-backend-express-main/boilerplate-backend-express-main/src/db/sequelize.ts
+++ server/boilerplate-backend-express-main/boilerplate-backend-express-main/src/db/sequelize.ts
@@ -3,17 +3,17 @@ const database: string = String(process.env.SQL_DATABASE);
 const username: string = String(process.env.SQL_USERNAME);
 const password: string = String(process.env.SQL_PASSWORD);
 const host: string = String(process.env.SQL_HOST);
-const dialect: Dialect =  process.env.SQL_DIALECT  as Dialect;
+const dialect: Dialect = process.env.SQL_DIALECT as Dialect;
 const sequelize = new Sequelize(database, username, password, {
     host,
     dialect,
 });
 export async function initSequelize() {
     try {
-      await sequelize.sync({ alter: true });
-      console.log('Database synced!');
+        await sequelize.sync({ alter: true });
+        console.log("Database synced!");
     } catch (error) {
-      console.error('Unable to connect to the database:', error);
+        console.error("Unable to connect to the database:", error);
     }
-  }
+}
 export default sequelize;
--- server/boilerplate-backend-express-main/boilerplate-backend-express-main/src/models/devices/devices.model.ts
+++ server/boilerplate-backend-express-main/boilerplate-backend-express-main/src/models/devices/devices.model.ts
@@ -2,55 +2,52 @@ import { DataTypes, Model, Optional } from "sequelize";
 import { v4 as uuidv4 } from "uuid";
 import sequelize from "../../db/sequelize";
 import { DeviceAttributes } from "./devices.entity";
+import UserModel from "../users/users.model";
 
 type DeviceCreationAttributes = Optional<DeviceAttributes, "deviceId">;
 
 class DeviceModel extends Model<DeviceAttributes, DeviceCreationAttributes> {
-  declare deviceId: string;
-  declare userId: string;
-  declare deviceName: string;
-  declare description: string;
-  declare category: string;
-  declare createdAt: Date;
-  declare updatedAt: Date;
+    declare deviceId: string;
+    declare userId: string;
+    declare deviceName: string;
+    declare description: string;
+    declare category: string;
+    declare createdAt: Date;
+    declare updatedAt: Date;
 }
 
 DeviceModel.init(
-  {
-    deviceId: {
-      type: DataTypes.STRING,
-      defaultValue: uuidv4, // Automatically generate UUIDs
-      primaryKey: true,
+    {
+        deviceId: {
+            type: DataTypes.STRING,
+            defaultValue: uuidv4, // Automatically generate UUIDs
+            primaryKey: true,
+        },
+        userId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        deviceName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
     },
-    // userId: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "Users",
-    //     key: "userId",
-    //   },
-    //   onUpdate: "CASCADE",
-    //   onDelete: "SET NULL",
-    // },
-    deviceName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-  },
-  {
-    sequelize,
-    modelName: "Device",
-    tableName: "Devices",
-  }
+    {
+        sequelize,
+        modelName: "Device",
+        tableName: "Devices",
+    }
 );
-
+UserModel.hasMany(DeviceModel, { foreignKey: "userId", constraints: true, as: "devices" });
+DeviceModel.belongsTo(UserModel, { foreignKey: "userId", constraints: false, as: "user" });
 export default DeviceModel;
